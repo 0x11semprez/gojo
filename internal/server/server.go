cb0aa8bf8c1f4eb1ce3e63ab879dde1ad3d47454
0x11semprez <traoresemprez@icloud.com>
2026-07-23 23:35:00 +0200

    fix(server): align NewServe and Server return types

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,30 +1,34 @@
 package server
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 	"time"
+
+	"gojo/internal/app"
 )
 
 type Server struct {
 	*http.Server
 }
 
-func NewServe(c *Config, handler http.Handler) *http.Server {
-	return &http.Server{
-		Addr:              c.Port,
-		Handler:           handler,
-		ReadTimeout:       5 * time.Second,
-		ReadHeaderTimeout: 2 * time.Second,
-		WriteTimeout:      5 * time.Second,
-		IdleTimeout:       60 * time.Second,
-		MaxHeaderBytes:    1 << 20,
+func NewServe(c *app.Config, handler http.Handler) Server {
+	return Server{
+		&http.Server{
+			Addr:              c.Port,
+			Handler:           handler,
+			ReadTimeout:       5 * time.Second,
+			ReadHeaderTimeout: 2 * time.Second,
+			WriteTimeout:      5 * time.Second,
+			IdleTimeout:       60 * time.Second,
+			MaxHeaderBytes:    1 << 20,
+		},
 	}
 }
 
-func (s *Server) StartServer() {
+func StartServer(s *Server) {
 	err := http.ListenAndServe(s.Addr, s.Handler)
 	if err != nil {
-		fmt.Errorf("There is a problem here: %w", err)
+		log.Fatal(err)
 	}
 }
