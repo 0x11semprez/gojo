a7093b66fa31ee8cdd33d27c9d592b00d447c48a
0x11semprez <traoresemprez@icloud.com>
2026-07-23 21:51:48 +0200

    feat(server): add http.Server wrapper

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,18 +1,30 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func NewServe(addr string, handler http.Handler) *http.Server {
+type Server struct {
+	*http.Server
+}
+
+func NewServe(c *Config, handler http.Handler) *http.Server {
 	return &http.Server{
-		Addr:              addr,
+		Addr:              c.Port,
 		Handler:           handler,
 		ReadTimeout:       5 * time.Second,
 		ReadHeaderTimeout: 2 * time.Second,
 		WriteTimeout:      5 * time.Second,
 		IdleTimeout:       60 * time.Second,
 		MaxHeaderBytes:    1 << 20,
+	}
+}
+
+func (s *Server) StartServer() {
+	err := http.ListenAndServe(s.Addr, s.Handler)
+	if err != nil {
+		fmt.Errorf("There is a problem here: %w", err)
 	}
 }
