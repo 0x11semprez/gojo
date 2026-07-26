311a849429132bf9b2a940b0586a0fd5a0583bba
0x11semprez <traoresemprez@icloud.com>
2026-07-26 10:35:31 +0200

    test(secret): add additional assertions

--- internal/middleware/generatesecret_test.go
+++ internal/middleware/generatesecret_test.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"encoding/hex"
+	"fmt"
 	"testing"
 )
 
@@ -13,6 +14,8 @@ func TestGenerateSecret(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	fmt.Printf("example of secret generated for the user: %v\n", generate)
+
 	got, err := hex.DecodeString(generate)
 	if err != nil {
 		t.Fatal(err)
@@ -21,6 +24,8 @@ func TestGenerateSecret(t *testing.T) {
 	want := 32
 
 	if len(got) != want {
-		t.Errorf("invalid secret length: got %d bytes, want %d", len(got), want)
+		t.Errorf("invalid secret length: got %d bytes, want %d\n", len(got), want)
 	}
+
+	fmt.Printf("gojo loves you, thanks for testing GenerateSecret() \n")
 }
