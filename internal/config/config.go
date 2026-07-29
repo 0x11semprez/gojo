b935bbe559b59feb220e2b427e87800f41409eac
0x11semprez <traoresemprez@icloud.com>
2026-07-29 16:06:07 +0200

    feat(config): add NewConfigTest for test environment

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,10 +8,12 @@ import (
 )
 
 type Config struct {
-	DatabaseURL          string
-	DatabaseURLMigration string
-	Port                 string
-	Jwt                  string
+	DatabaseURL              string
+	DatabaseURLMigration     string
+	DatabaseURLTest          string
+	DatabaseURLMigrationTest string
+	Port                     string
+	Jwt                      string
 }
 
 func NewConfig() (*Config, error) {
@@ -21,7 +23,20 @@ func NewConfig() (*Config, error) {
 	}
 	return &Config{
 		DatabaseURL:          os.Getenv("DATABASE_URL"),
-		DatabaseURLMigration: os.Getenv("DatabaseURLMigration"),
+		DatabaseURLMigration: os.Getenv("DATABASE_URL_MIGRATION"),
+		Port:                 os.Getenv("PORT"),
+		Jwt:                  os.Getenv("JWT_TOKEN"),
+	}, nil
+}
+
+func NewConfigTest() (*Config, error) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		return nil, errors.New("error loading .env.test file")
+	}
+	return &Config{
+		DatabaseURL:          os.Getenv("DATABASE_URL_TEST"),
+		DatabaseURLMigration: os.Getenv("DATABASE_URL_MIGRATION_TEST"),
 		Port:                 os.Getenv("PORT"),
 		Jwt:                  os.Getenv("JWT_TOKEN"),
 	}, nil
