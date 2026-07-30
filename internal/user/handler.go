abd69d5450569635cf81e6b65cf2b50e143292fa
0x11semprez <traoresemprez@icloud.com>
2026-07-30 18:00:44 +0200

    feat(user): add CreateUser handler

--- internal/user/handler.go
+++ internal/user/handler.go
@@ -1,9 +1,13 @@
 package user
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
+
+	"gojo/internal/app"
+	"gojo/internal/middleware"
 )
 
 func Health(w http.ResponseWriter, r *http.Request) {
@@ -11,4 +15,15 @@ func Health(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func CreateUser(db *app.App) (string, error) {
+	secret, err := middleware.GenerateSecret()
+	if err != nil {
+		errors.New("cannot create user")
+	}
+
+	fmt.Print("your secret is: %v", secret)
+
+	return secret, nil
 }
