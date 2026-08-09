6ec3a8ae5c7dad2116eaffeb4c7a60cf8e51db9e
0x11semprez <traoresemprez@icloud.com>
2026-08-09 14:37:26 +0200

    refactor(config): remove unused config code

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,35 +8,20 @@ import (
 )
 
 type Config struct {
-	DatabaseURL              string
-	DatabaseURLMigration     string
-	DatabaseURLTest          string
-	DatabaseURLMigrationTest string
-	Port                     string
-	Jwt                      string
+	DatabaseURL          string
+	DatabaseURLMigration string
+	Port                 string
+	Jwt                  string
 }
 
-func (c Config) NewConfig() (*Config, error) {
+func (c Config) New() (*Config, error) {
 	err := godotenv.Load("../../.env")
 	if err != nil {
-		return nil, errors.New("error loading .env file")
+		return nil, errors.New("cannot laod .env file")
 	}
 	return &Config{
 		DatabaseURL:          os.Getenv("DATABASE_URL"),
 		DatabaseURLMigration: os.Getenv("DATABASE_URL_MIGRATION"),
-		Port:                 os.Getenv("PORT"),
-		Jwt:                  os.Getenv("JWT_TOKEN"),
-	}, nil
-}
-
-func (c Config) NewConfigTest() (*Config, error) {
-	err := godotenv.Load("../../.env")
-	if err != nil {
-		return nil, errors.New("error loading .env.test file")
-	}
-	return &Config{
-		DatabaseURL:          os.Getenv("DATABASE_URL_TEST"),
-		DatabaseURLMigration: os.Getenv("DATABASE_URL_MIGRATION_TEST"),
 		Port:                 os.Getenv("PORT"),
 		Jwt:                  os.Getenv("JWT_TOKEN"),
 	}, nil
