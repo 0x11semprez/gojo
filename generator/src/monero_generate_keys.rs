1bd6cd4912f664d7e38f189f0c4324769b972418
0x11semprez <traoresemprez@icloud.com>
2026-08-13 00:57:54 +0200

    feat(generator): generate moneo keys

--- generator/src/monero_generate_keys.rs
+++ generator/src/monero_generate_keys.rs
@@ -1,7 +1,40 @@
-use monero::util::key::{PrivateKey, from_slice};
-use monero::util::Error;
+use machine_info::Machine;
+use monero::{Hash, PrivateKey, PublicKey};
+use rand::rngs::OsRng;
+use rand::RngCore;
 
+use crate::all_computer_infos::cpu_info;
 
-pub fn generate_private_key() -> Result<PublicKey, error> {
+#[derive(Debug)]
+pub struct MoneroKeys {
+    pub private_spend_key: PrivateKey,
+    pub public_spend_key: PublicKey,
+    pub private_view_key: PrivateKey,
+    pub public_view_key: PublicKey,
+}
 
+pub fn generate_private_key(machine: &mut Machine) -> MoneroKeys {
+    let processor = cpu_info(machine);
+
+    let mut seed_material = Vec::new();
+    seed_material.extend_from_slice(processor.vendor.as_bytes());
+    seed_material.extend_from_slice(processor.brand.as_bytes());
+    seed_material.extend_from_slice(&processor.frequency.to_le_bytes());
+
+    let mut random_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut random_bytes);
+    seed_material.extend_from_slice(&random_bytes);
+
+    let private_spend_key = Hash::hash_to_scalar(&seed_material);
+    let public_spend_key = PublicKey::from_private_key(&private_spend_key);
+
+    let private_view_key = Hash::hash_to_scalar(private_spend_key.as_bytes());
+    let public_view_key = PublicKey::from_private_key(&private_view_key);
+
+    MoneroKeys {
+        private_spend_key,
+        public_spend_key,
+        private_view_key,
+        public_view_key,
+    }
 }
