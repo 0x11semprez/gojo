9e310d0143553ad7b4502d709656c9e5b3dc654c
0x11semprez <traoresemprez@icloud.com>
2026-08-13 00:58:19 +0200

    feat(generaor): check in main

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,7 +1,11 @@
 mod all_computer_infos;
+mod bitcoin_generate_keys;
+mod monero_generate_keys;
 
 use all_computer_infos::{cpu_info, cpu_status, total_cores, total_memory};
+use bitcoin_generate_keys::generate_private_key as generate_bitcoin_keys;
 use machine_info::Machine;
+use monero_generate_keys::generate_private_key as generate_monero_keys;
 
 fn main() {
     let mut machine = Machine::new();
@@ -17,4 +21,10 @@ fn main() {
 
     let cores = total_cores(&mut machine);
     println!("Total cores: {}", cores);
+
+    let monero_keys = generate_monero_keys(&mut machine);
+    println!("{:?}", monero_keys);
+
+    let bitcoin_keys = generate_bitcoin_keys(&mut machine);
+    println!("{:?}", bitcoin_keys);
 }
