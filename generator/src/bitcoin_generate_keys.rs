3476212b9770a77c058088a88804bb35e9e531e4
0x11semprez <traoresemprez@icloud.com>
2026-08-13 00:57:31 +0200

    feat(generator): generate bitcoin keys

---
+++ generator/src/bitcoin_generate_keys.rs
@@ -0,0 +1,56 @@
+use bitcoin::hashes::{sha256, Hash};
+use bitcoin::secp256k1::{Secp256k1, SecretKey};
+use bitcoin::{Address, CompressedPublicKey, Network, NetworkKind, PrivateKey};
+use machine_info::Machine;
+use rand::rngs::OsRng;
+use rand::RngCore;
+
+use crate::all_computer_infos::cpu_info;
+
+#[derive(Debug)]
+pub struct BitcoinKeys {
+    pub private_key: PrivateKey,
+    pub public_key: CompressedPublicKey,
+    pub address: Address,
+}
+
+pub fn generate_private_key(machine: &mut Machine) -> BitcoinKeys {
+    let processor = cpu_info(machine);
+
+    let mut seed_material = Vec::new();
+    seed_material.extend_from_slice(processor.vendor.as_bytes());
+    seed_material.extend_from_slice(processor.brand.as_bytes());
+    seed_material.extend_from_slice(&processor.frequency.to_le_bytes());
+
+    let mut random_bytes = [0u8; 32];
+    OsRng.fill_bytes(&mut random_bytes);
+    seed_material.extend_from_slice(&random_bytes);
+
+    // Unlike Monero's scalar field, secp256k1 rejects 0 and values >= the curve
+    // order. That is astronomically unlikely (~2^-128) with a SHA-256 output, but
+    // we bump a nonce and rehash instead of unwrapping, just in case.
+    let mut nonce: u32 = 0;
+    let secret_key = loop {
+        let mut data = seed_material.clone();
+        data.extend_from_slice(&nonce.to_le_bytes());
+        let hash = sha256::Hash::hash(&data);
+        match SecretKey::from_slice(hash.as_byte_array()) {
+            Ok(sk) => break sk,
+            Err(_) => nonce += 1,
+        }
+    };
+
+    let private_key = PrivateKey::new(secret_key, NetworkKind::Main);
+
+    let secp = Secp256k1::new();
+    let public_key = CompressedPublicKey::from_private_key(&secp, &private_key)
+        .expect("private key is always constructed as compressed");
+
+    let address = Address::p2wpkh(&public_key, Network::Bitcoin);
+
+    BitcoinKeys {
+        private_key,
+        public_key,
+        address,
+    }
+}
